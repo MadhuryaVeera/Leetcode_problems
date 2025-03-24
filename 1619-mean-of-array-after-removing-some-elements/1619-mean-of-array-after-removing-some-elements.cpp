e44c7aac67decc9720687192c2889917635a1a26
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2025-03-24 21:07:25 +0530

    Time: 0 ms (100%), Space: 13.3 MB (16.72%) - LeetHub

---
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double trimMean(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int n = arr.size();
+        int k = n/20;
+        double sum=0,cnt=0;
+        for(int i=k; i < n-k; i++)
+        {
+            sum+=arr[i];
+            cnt++;
+        }
+        return sum/cnt;
+    }
+};
