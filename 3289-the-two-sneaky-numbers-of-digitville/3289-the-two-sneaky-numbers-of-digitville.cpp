85a21f322438ae9a0623c96bc6de6167e97565eb
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2025-10-31 23:03:01 +0530

    Time: 8 ms (9.16%), Space: 26.4 MB (15.56%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        unordered_map<int,int> cftwo; // count_frequency-of twice of element
+        unordered_map<int,int>ctfreq;
+        for(int num:nums) ctfreq[num]++;
 
-        // counting the frequnecy of each number 
-        //{
-                // 7: 1, 1: 1, 5: 2, 4: 2, 3: 1,
-                //  6: 1, 0: 1, 9: 1, 8: 1, 2: 1
-            // }
+        vector<int>res;
 
-        for(auto num:nums){
-                cftwo[num]++;
+        for(auto pair:ctfreq)
+        {
+            if(pair.second==2) res.push_back(pair.first);
         }
-
-        vector<int> result; //for resultant vector that we have to return 
-
-        for(auto pair:cftwo){     //after counting its frequnecy then if it appears twice the push into the resultant vector called result.
-
-           //.second checks the frequency of the vector(values )
-            if(pair.second==2){
-                result.push_back(pair.first); // .first checks the keys in the vector
-            }
-        }
-        return result;
-
+        return res;
     }
 };
