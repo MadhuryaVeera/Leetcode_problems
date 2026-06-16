e314ab91b22330d7d490f796687da6d650b36533
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2026-06-16 08:39:42 +0530

    Time: 4 ms (76.97%), Space: 23.3 MB (82.24%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def processStr(self, s: str) -> str:
+        res =""
+        for ch in s:
+            if ch.isalpha():
+                res+=ch
+            elif ch=="*":
+                if res:
+                    res=res[:-1]
+            elif ch=="#":
+                res+=res
+            else:
+                res=res[::-1]
+        return res
+
+
+        
