32340336dac191f352e0aa7a6a58190eb2445a2c
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2026-06-19 10:11:18 +0530

    Time: 0 ms (100%), Space: 19.2 MB (52.19%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -1,9 +1,17 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
+        # Optimal approach which is prefix sum
         current=0
         highest=0
         for g in gain:
-            current+=g
+            current+=g 
+            # current =0 
+            # curent =current+g=0+(-5)=-5
+            #
             highest=max(highest,current)
+            # max(0,-5) =0
+            
+            # then agaian the loop will contiune and then we got answer of it .
         return highest
+            # then finnaly he want to return that element in it 
         
