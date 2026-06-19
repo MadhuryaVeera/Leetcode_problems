6a718c5cd5a4f8f346ea1f41d13f4491bb7e0923
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2026-06-19 10:08:57 +0530

    Time: 0 ms (100%), Space: 19.3 MB (15.4%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -1,32 +1,9 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        # Brute Force approach 
-        altitude=[0] # at a point 0 every start elemnt has at 0 index 0 is fix we are taking
-
+        current=0
+        highest=0
         for g in gain:
-            altitude.append(altitude[-1]+g)
-            #[-5,1,5,0,-7]
-
-            #altitude[-1] =0
-            #altitide[-1]+g = 0+(-5) = -5
-
-            #altitude[-1]=-5
-            #altitude[-1]+g=-5+1=-4
-
-            #altitude[-1] =-4
-            #altitide[-1]+g = -4+(5) = 1
-
-            #altitude[-1]=1
-            #altitude[-1]+g = 1+0 =1
-            
-            #altitude[-1] =1
-            #altitide[-1]+g = 1+(-7) = -6
-
-            # then total array we can obtain is [-5,-4,1,1,-6]
-
-            # then we append that to altitude which can give the array is 
-            #[0,-5,-4,1,1-6]
-            # then  we find the max element in it which is 1.
-        return max(altitude)
-
+            current+=g
+            highest=max(highest,current)
+        return highest
         
